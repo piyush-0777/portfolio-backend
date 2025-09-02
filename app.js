8f31bf368d13682861369116a32d8027d543c181
piyush-0077 <khandharpiyush5@gmail.com>
2025-09-02 13:01:27 +0530

    remove some

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const projectRouter = require('./routes/projects');
 const mongoDB_connection = require('./config/db')
 const path = require('path')
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 // console.log(_dirname);
 
 
@@ -21,10 +21,10 @@ app.use(cors({
 }))
 app.use('/project' , projectRouter)
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-})
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.get("*", (req,res)=>{
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// })
 
 app.listen(port , () =>{
     console.log(`your app is runing at the port ${port}`);
